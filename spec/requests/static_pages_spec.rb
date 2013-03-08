6e800c133f532a02053c5fc2f230e8bf2c917e72
Michael Hartl <michael@michaelhartl.com>
2013-03-08 10:35:52 -0800

    Use have_title, have_content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,12 @@ describe "Static pages" do
 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_selector('h1', text: 'Sample App')
+      expect(page).to have_content('Sample App')
     end
     
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App | Home",
-                        visible: false)
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -21,14 +19,12 @@ describe "Static pages" do
 
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_selector('h1', text: 'Help')
+      expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App | Help",
-                        visible: false)
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -36,14 +32,12 @@ describe "Static pages" do
 
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_selector('h1', text: 'About Us')
+      expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | About Us",
-                    visible: false)
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
