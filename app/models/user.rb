eb02c13b53025e127c86322dbc1559dcaa3b873a
Michael Hartl <michael@michaelhartl.com>
2013-03-16 13:08:24 -0700

    Add better email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true,
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password_confirmation, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,6 +54,27 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end      
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end      
+    end
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
