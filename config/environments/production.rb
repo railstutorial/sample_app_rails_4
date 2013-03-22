1e45b8f039e654d9e907c55f1bf4e9b6dee5aab6
Michael Hartl <michael@michaelhartl.com>
2013-03-21 17:44:19 -0700

    Remove obsolete comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,6 @@ SampleApp::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
