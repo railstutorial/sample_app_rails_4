ba932b2559b7f3a5178b8581c0fdef5f9cc3bdd2
Michael Hartl <michael@michaelhartl.com>
2013-06-10 18:53:06 -0700

    Remove unneeded password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,10 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
 
   def feed
@@ -27,13 +26,13 @@ class User < ActiveRecord::Base
   def follow!(other_user)
     relationships.create!(followed_id: other_user.id)
   end
-  
+
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
-  
+
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
