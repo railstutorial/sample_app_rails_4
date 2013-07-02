324d5ced5fcbadd91fc5993d72f29b0ff8261170
Michael Hartl <michael@michaelhartl.com>
2013-07-01 17:48:01 -0700

    Make anal changes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,7 @@ module SessionsHelper
   end
 
   def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
+    remember_token  = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
