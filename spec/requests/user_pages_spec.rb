1b1d47911cc54963ec73a7d62386e5a2bc6b5c42
Michael Hartl <michael@michaelhartl.com>
2013-07-12 15:20:13 -0700

    Add match: :first

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,7 +41,9 @@ describe "User pages" do
 
         it { should have_link('delete', href: user_path(User.first)) }
         it "should be able to delete another user" do
-          expect { click_link('delete') }.to change(User, :count).by(-1)
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
         end
         it { should_not have_link('delete', href: user_path(admin)) }
       end
