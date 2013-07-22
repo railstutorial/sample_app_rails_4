f59e23ba7cf1b387d082b380ff7d9ba7c650403e
Michael Hartl <michael@michaelhartl.com>
2013-07-22 12:31:50 -0700

    Use have_selector to test for contact header

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,7 +19,7 @@ describe "Static pages" do
         sign_in user
         visit root_path
       end
-      
+
       it "should render the user's feed" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
@@ -41,22 +41,22 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
-    
+
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
-    
+
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    
-    it { should have_content('Contact') }
+
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 
