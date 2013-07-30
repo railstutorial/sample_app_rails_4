dbbbca6cf28be816bc9a833b3ba0c085fc214dbe
Michael Hartl <michael@michaelhartl.com>
2013-07-29 18:22:33 -0700

    Use destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ActiveRecord::Base
   end
 
   def unfollow!(other_user)
-    relationships.find_by(followed_id: other_user.id).destroy
+    relationships.find_by(followed_id: other_user.id).destroy!
   end
 
   private
