7ac31a1078be88e9f9abc958e66fdf41e0f3beae
Michael Hartl <michael@michaelhartl.com>
2013-09-05 18:29:46 -0700

    Switch to a direct 'get' request

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -128,9 +128,10 @@ describe "Authentication" do
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user, no_capybara: true }
 
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_title(full_title('Edit user')) }
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
       end
 
       describe "submitting a PATCH request to the Users#update action" do
