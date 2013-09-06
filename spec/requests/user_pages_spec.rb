274fa381ffbfddae13af737bb35777d7642ada2d
Michael Hartl <michael@michaelhartl.com>
2013-09-05 18:38:23 -0700

    Have test pass for the right reason

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -201,7 +201,10 @@ describe "User pages" do
         { user: { admin: true, password: user.password,
                   password_confirmation: user.password } }
       end
-      before { patch user_path(user), params }
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
       specify { expect(user.reload).not_to be_admin }
     end
   end
