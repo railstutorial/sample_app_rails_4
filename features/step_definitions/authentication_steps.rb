0fe4f360ad45426aedf9693e7c61dd7f7d1c3b19
Michael Hartl <michael@michaelhartl.com>
2013-10-18 21:47:19 -0700

    Switch to she/her in the Capybara story

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -2,11 +2,11 @@ Given /^a user visits the signin page$/ do
   visit signin_path
 end
 
-When /^he submits invalid signin information$/ do
+When /^she submits invalid signin information$/ do
   click_button "Sign in"
 end
 
-Then /^he should see an error message$/ do
+Then /^she should see an error message$/ do
   expect(page).to have_selector('div.alert.alert-error')
 end
 
@@ -17,14 +17,14 @@ end
 
 When /^the user submits valid signin information$/ do
   fill_in "Email",    with: @user.email
-  fill_in "Password", with: @user.password 
+  fill_in "Password", with: @user.password
   click_button "Sign in"
 end
 
-Then /^he should see his profile page$/ do
+Then /^she should see her profile page$/ do
   expect(page).to have_title(@user.name)
 end
 
-Then /^he should see a signout link$/ do
+Then /^she should see a signout link$/ do
   expect(page).to have_link('Sign out', href: signout_path)
 end
