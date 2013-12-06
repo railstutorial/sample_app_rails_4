1165f3d7aee092d2198a74f05715681025fba175
Gaurish Sharma <contact@gaurishsharma.com>
2013-12-06 21:37:24 +0530

    Use to downcase!(with bang) to not create a new object
    This does the same thing and but is bit simpler to read.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
                                    class_name:  "Relationship",
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
-  before_save { self.email = email.downcase }
+  before_save { self.email.downcase! }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
