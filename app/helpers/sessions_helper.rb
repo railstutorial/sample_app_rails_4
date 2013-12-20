dc93dbd50624e59d3d002597cd4de7b910a066cb
Michael Hartl <michael@michaelhartl.com>
2013-12-19 19:10:58 -0800

    Change cookie upon sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,6 +32,8 @@ module SessionsHelper
   end
 
   def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.encrypt(User.new_remember_token))
     self.current_user = nil
     cookies.delete(:remember_token)
   end
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@
         # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
         # config.i18n.default_locale = :de
         # I18n.enforce_available_locales = true
+        I18n.enforce_available_locales = true
 
         config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
       end
