07bae25cf7880b0355bbe73b8853f1a8e349ca7d
Michael Hartl <michael@michaelhartl.com>
2014-04-17 12:07:54 -0700

    Change from hash to digest

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,7 +5,7 @@ def sign_in(user, options={})
     # Sign in when not using Capybara as well.
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.hash(remember_token))
+    user.update_attribute(:remember_token, User.digest(remember_token))
   else
     visit signin_path
     fill_in "Email",    with: user.email
