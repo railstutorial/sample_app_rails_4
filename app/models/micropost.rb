3d33079071efad5829ef6c1355f7e492b391d0e9
Paul McReynolds <pauljm@gmail.com>
2015-02-25 18:28:06 -0700

    Spec and stub #html_content

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,10 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 
+  def html_content
+    content
+  end
+
   # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -27,4 +27,33 @@ describe Micropost do
     before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
   end
+
+  describe "#html_content" do
+
+    it "should escape HTML chars" do
+
+      @micropost.content = "<b>I want to be bold!</b>"
+
+      expect( @micropost.html_content ).to eq "&lt;b&gt;I want to be bold!&lt;/b&gt;"
+
+    end
+
+    it "should hyperlink HTTP URLs" do
+
+      @micropost.content = "Check out http://sparkboulder.com"
+
+      expect( @micropost.html_content ).to eq "Check out <a href=\"http://sparkboulder.com\">http://sparkboulder.com</a>"
+
+    end
+
+    it "should hyperlink HTTPS URLs" do
+
+      @micropost.content = "Check out https://www.conspire.com"
+
+      expect( @micropost.html_content ).to eq "Check out <a href=\"https://www.conspire.com\">https://www.conspire.com</a>"
+
+    end
+
+  end
+
 end
