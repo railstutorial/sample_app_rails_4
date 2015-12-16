fbaa12f4f11b319c8f5c46a1cc796a2594776db9
Luke Roberts <email@luke-roberts.co.uk>
2015-12-16 15:27:34 -0800

    dumb space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
 end
