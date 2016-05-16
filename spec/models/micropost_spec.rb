2d1d0ad0910546973869114f2584a73d693eebab
Steven Boyd <steven.boyd@appfolio.com>
2016-05-16 10:34:26 -0700

    sb - use single quotes for non-interpelated strings in micropost spec

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -5,7 +5,7 @@ describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
   before do
     # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+    @micropost = Micropost.new(content: 'Lorem ipsum', user_id: user.id)
   end
 
   subject { @micropost }
@@ -13,23 +13,23 @@ describe Micropost do
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
   
-  describe "can have an image url" do
+  describe 'can have an image url' do
     before { @micropost.image_url = 'https://www.nasa.gov/mission_pages/apollo/images.html'}
     it { should be_valid }
   end
 
-  describe "when user_id is not present" do
+  describe 'when user_id is not present' do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
 
-  describe "with blank content" do
-    before { @micropost.content = " " }
+  describe 'with blank content' do
+    before { @micropost.content = ' ' }
     it { should_not be_valid }
   end
 
-  describe "with content that is too long" do
-    before { @micropost.content = "a" * 141 }
+  describe 'with content that is too long' do
+    before { @micropost.content = 'a' * 141 }
     it { should_not be_valid }
   end
 end
