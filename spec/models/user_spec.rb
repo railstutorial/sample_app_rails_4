bad31a173bec2a463bdb5533908dc9236580b649
hubpixie <venusjanne@venusjannes-Mac-mini.local>
2017-07-16 18:15:46 +0900

    fix a error. when running rspec spec/models/ ,  Rails reports ---Failure/Error: specify.. ()expected false to respond to `false?` or perhaps you meant `be false` or `be_falsey`

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -115,7 +115,7 @@ describe User do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
       it { should_not eq user_for_invalid_password }
-      specify { expect(user_for_invalid_password).to be_false }
+      specify { expect(user_for_invalid_password).to be_falsey }
     end
   end
 
